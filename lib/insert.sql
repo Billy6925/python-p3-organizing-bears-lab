9fba440f56f966e7262988a784349d24154dd26e
Billy Eskein <billyeskein@gmail.com>
2025-02-26 05:55:28 +0300

    Update lib/sql_queries.py

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES("Mr Chocolate", 10, "M", "black", "medium", 1);
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES("Rowdy", 7, "M", "stripped", "high", 1);
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES("Tabitha", 15, "F", "white", "low", 1);
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES("Sergeant Brown", 13, "M", "stripped", "medium", 1);
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES("Melissa", 3, "F", "white", "medium", 1);
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES("Grinch", 10, "M", "black", "hight", 0);
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES("Wendy", 10, "F", "stripped", "low", 1);
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES(NULL, 1, "M", "black", "medium", 1);
